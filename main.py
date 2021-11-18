226547ab4e61f27391d28ae3c0d8e812199354cd
Doyeon <d_kim120803@fanshaweonline.ca>
2021-11-18 12:07:16 +0900

    adding Ujson

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import requests
-r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-r.status_code
-200
-r.headers['content-type']
-'application/json; charset=utf8'
-r.encoding
-'utf-8'
-r.text
-'{"type":"User"...'
-r.json()
-{'disk_usage': 368627, 'private_gists': 484, ...}
+import ujson
+
+ujson.dumps([{"key": "value"}, 81, True])
+'[{"key":"value"},81,true]'
+ujson.loads("""[{"key": "value"}, 81, true]""")
+[{'key': 'value'}, 81, True]
