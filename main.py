d942cb355a37db49a7ae77cc19fc010fd78959e4
Doyeon <d_kim120803@fanshaweonline.ca>
2021-11-18 12:13:42 +0900

    adding Deteutil

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import ujson
+from dateutil.relativedelta import *
+from dateutil.easter import *
+from dateutil.rrule import *
+from dateutil.parser import *
+from datetime import *
+now = parse("Sat Oct 11 17:13:46 UTC 2003")
+today = now.date()
+year = rrule(YEARLY,dtstart=now,bymonth=8,bymonthday=13,byweekday=FR)[0].year
+rdelta = relativedelta(easter(year), today)
+print("Today is: %s" % today)
+Today is: 2003-10-11
+print("Year with next Aug 13th on a Friday is: %s" % year)
+Year with next Aug 13th on a Friday is: 2004
+print("How far is the Easter of that year: %s" % rdelta)
+How far is the Easter of that year: relativedelta(months=+6)
+print("And the Easter of that year is: %s" % (today+rdelta))
+And the Easter of that year is: 2004-04-11
 
-ujson.dumps([{"key": "value"}, 81, True])
-'[{"key":"value"},81,true]'
-ujson.loads("""[{"key": "value"}, 81, true]""")
-[{'key': 'value'}, 81, True]
