4794a1411b1c0f00f1235e2510dff273112bd292
Doyeon <d_kim120803@fanshaweonline.ca>
2021-11-18 12:01:09 +0900

    adding Requests

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import jmespath
-
-expression = jmespath.compile('foo.bar')
-expression.search({'foo': {'bar': 'baz'}})
-'baz'
-expression.search({'foo': {'bar': 'other'}})
-'other'
+import requests
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
+200
+r.headers['content-type']
+'application/json; charset=utf8'
+r.encoding
+'utf-8'
+r.text
+'{"type":"User"...'
+r.json()
+{'disk_usage': 368627, 'private_gists': 484, ...}
